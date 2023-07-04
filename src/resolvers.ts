7ae215809f3419c806bf56d7de4f1c81a061fce5
AMMAR ABUBAKAR <you@example.com>
2023-07-04 09:06:40 +0100

    moved files to src and changed them to ts

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,28 +1,11 @@
-interface Book {
-  id: string;
-  title: string;
-  author: string;
-}
-
-const books: Book[] = [
-  { id: '1', title: 'Book 1', author: 'Author 1' },
-  { id: '2', title: 'Book 2', author: 'Author 2' },
+const products = [
+  { id: '1', name: 'Product 1', price: 9.99, description: 'Description 1' },
+  { id: '2', name: 'Product 2', price: 19.99, description: 'Description 2' },
 ];
 
 const resolvers = {
   Query: {
-    books: (): Book[] => books,
-  },
-  Mutation: {
-    addBook: (_: any, args: { title: string; author: string }): Book => {
-      const newBook: Book = {
-        id: String(books.length + 1),
-        title: args.title,
-        author: args.author,
-      };
-      books.push(newBook);
-      return newBook;
-    },
+    products: () => products,
   },
 };
 
